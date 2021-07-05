d11722d9120205efecdadafc53d2684963d90f45
Daniel Ghimpu <smeu.valentina33@gmail.com>
2021-07-05 17:07:13 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello there!</h1>
-      <button onClick={() => (alert("Hello world!"))} data-cy="btn-hello">Click me!</button>
+
     </div>
   );
 }
